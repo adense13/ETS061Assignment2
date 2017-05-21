7888fae48e8a7ccd6aa81921ee70dbd8ee251e25
Adrian <adense13@gmail.com>
2017-05-21 14:13:41 +0200

    cleaned useless coade

--- LP_1.m
+++ LP_1.m
@@ -1,20 +1,3 @@
-
-
-
-%%
-syms x1 x2 x3 x4 x5
-z = (4*x1)+(3*x2)+(2*x3)+(2*x4)+(1*x5);
-eqn1 = x1 <= 16;
-eqn2 = x2 <= 2;
-eqn3 = (x1+x2+x3) <= 34;
-eqn4 = (x4+x5) <= 28;
-eqn5 = 2*x1 <= 36;
-eqn6 = (2*x2)+(2*x3)+(2*x4)+x5 <= 216;
-eqn7 = (0.2*x1) + x2 + (0.5*x4) <= 18;
-[A,B] = equationsToMatrix([eqn1, eqn2, eqn2, eqn4, eqn5, eqn6, eqn7], [x1, x2, x3, x4, x5])
-
-%%
-
 clc
 clear all
 
@@ -45,52 +28,64 @@ A = [2 0 0 0 0;
       0;
       0];
   
- lb = zeros(5, 1)
- %options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'iter');
- %options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'off');
- %options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
- options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
- [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options)
- 
- %1d
- 
- oldProfit = 0
- newProfit = -c*x
- 
- profitArray = [newProfit]
+ lb = zeros(5, 1);
+
+%--------------------------------
+%1b------------------------------
+%--------------------------------
+
+%options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'iter');
+%options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'off');
+%options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
+options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
+[x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options);
+x
+
+%--------------------------------
+%1d------------------------------
+%--------------------------------
+
+oldProfit = 0;
+profit = -c*x
+
+profitArray = [profit]
  
  while true
-    oldProfit = newProfit;
+    oldProfit = profit;
     
     b = b + b_incr;
     
-    
     [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options);
-    newProfit = -c*x
-    profitArray=[profitArray newProfit];
-    %b_incr = b_incr+b_incr;
-    if((newProfit-oldProfit) < 0.1), break, end
+    profit = -c*x
+    profitArray=[profitArray profit];
+    if((profit-oldProfit) < 0.1), break, end
  end
 plot(profitArray)
 ylabel('Total Profit (1000 SEK)')
-% 1e
-b = [36;
-      216;
-      18;
-      16;
-      2;
-      34;
-      28]
 
+%--------------------------------
+%1e------------------------------
+%--------------------------------
+%First let's start by resetting all values to their initial state
+b = [36;
+     216;
+     18;
+     16;
+     2;
+     34;
+     28]; %reset our b matrix, since we messed with that in 1d
   
-[x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options)
-startC = c;
-startX = x;
+[x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options); %to reset our x values
+startC = c; %values to reset to for the beta loop
+startX = x; %values to reset to for the beta loop
+
+%Now let's start with the actual task 1e
 oldX = x;
 price_incr = -0.01;
 alfa = 4;
 beta = 4;
-x
+
+%let's find the lower interval bound
 while true
     c = c - [price_incr 0 0 0 0];
     alfa = alfa + price_incr;
@@ -98,8 +93,9 @@ while true
     if(isequal(oldX, x) == false), break, end
     oldX = x;
 end
-alfa
+alfa %lower bound
 
+%let's find the upper interval bound
 x = startX;
 c = startC;
 while true
@@ -107,6 +103,7 @@ while true
     beta = beta - price_incr;
     [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options);
     if(isequal(oldX, x) == false), break, end
+    %if(beta > 10), break, end %can be used to break loop when beta gets way too big
     oldX = x;
 end
-beta
+beta %upper bound
